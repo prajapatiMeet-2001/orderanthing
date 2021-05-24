f9c370c79d3f23e2ceca4e6948c3842dbf96220e
Meet <55619489+prajapatiMeet-2001@users.noreply.github.com>
2021-05-24 09:28:31 +0530

    Add files via upload

--- schema/deliveryperson.js
+++ schema/deliveryperson.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');  
 var DeliverPersonSchema = new mongoose.Schema({  
-  name: {type:String,required:true,unique : true},
-  phone: {type:String,required:true},
+  name: {type:String,required:true},
+  phone: {type:String,required:true,unique : true},
 });
 mongoose.model('DeliveryPerson', DeliverPersonSchema);
 
--- schema/users.js
+++ schema/users.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');  
 var UserSchema = new mongoose.Schema({  
-  name: {type:String,required:true,unique : true},
-  phone: {type:String,required:true},
+  name: {type:String,required:true},
+  phone: {type:String,required:true,unique : true},
   password: {type:String,required:true}
 });
 mongoose.model('User', UserSchema);
